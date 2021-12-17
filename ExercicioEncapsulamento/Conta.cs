d4fd19887965cafd58929105713a6ac90a552ee5
lucas barros <lucasbarrosrock@hotmail.com>
2021-12-17 19:41:53 -0300

    Add project files.

---
+++ ExercicioEncapsulamento/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioEncapsulamento
+{
+    internal class Conta
+    {
+        private string nome;
+        private double saldo;
+        private string senha;
+        private double limite;
+
+        public Conta(string nome, double saldo, string senha)
+        {
+            this.nome = nome;
+            this.saldo = saldo;
+            this.senha = senha;
+        }
+              
+     
+        public void Depositar(double valor) { saldo = saldo + valor; }
+        public void Saque(double valor) { saldo = saldo - valor; }
+        public void AlterarLimite(double valor) 
+        { 
+           if(saldo * 2 > valor)
+            {
+                limite = valor;
+            }
+            else
+            {
+                Console.WriteLine("Não foi possível colocar o limite solicitado! O limite só pode ser até duas vezes o saldo em conta!");
+            }
+        }
+        public override string ToString()
+        {
+            return $"Nome do Titular: " + nome + "\nSaldo em conta: " + saldo + "\nLimite do cartão de crédito: " + limite;
+        }
+    }
+}
---
+++ ExercicioEncapsulamento/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExercicioEncapsulamento;
+
+Console.Write("Digitar o nome do Títular: ");
+string nome = Console.ReadLine();
+
+Console.Write("Digite saldo em conta: ");
+double saldo = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a senha da conta: ");
+string senha = Console.ReadLine();
+
+Conta conta = new Conta(nome,saldo,senha);
+Console.WriteLine(conta);
+
+//-------- Função de Deposito ------------------
+Console.Write("Digite um valor para ser depositado: ");
+double deposito = double.Parse(Console.ReadLine());
+conta.Depositar(deposito);
+Console.WriteLine(conta);
+
+//-------- Função de saque ------------------
+Console.Write("Digite um valor para ser sacado: ");
+double saque = double.Parse(Console.ReadLine());
+conta.Saque(saque);
+Console.WriteLine(conta);
+
+//-------- Função alterar o Limite do cartão de Crédito -----------------
+Console.Write("Digite o limite para seu cartão de crédito: ");
+double limite = double.Parse(Console.ReadLine());
+conta.AlterarLimite(limite);
+Console.WriteLine(conta);
+
+
